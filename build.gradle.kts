35f9dbb0609275884421a8f5b85de5906473d10d
magtamas <mag.tamas@codeyard.eu>
2023-10-30 10:36:14 +0100

    Add maven plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,14 @@
 plugins {
     id("com.android.application") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("maven-publish")
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.magtamas"
+            artifactId = "library"
+            version = "0.0.2"
+        }
+    }
 }
